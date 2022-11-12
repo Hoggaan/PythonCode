5b4b17250c51cc1bc626b41a4968c62add5a2341
Hoggaan <mahuraanhoggaan@gmail.com>
2022-11-12 19:06:58 +0530

    Dailly Updates

--- tutorials/calendarModule.py
+++ tutorials/calendarModule.py
@@ -15,6 +15,22 @@ dayoftheweek = calendar.weekday(2022, 11, 7)
 # print(dayoftheweek)
 
 numdays = calendar.monthrange(2022, 12)
-print(numdays)
+# print(numdays)
 weekaday = calendar.weekday(2022, 12, 1)
-print(weekaday)
+# print(weekaday)
+
+weeksInMatrx = calendar.monthcalendar(2022, 12)
+# print(weeksInMatrx)
+# for i in weeksInMatrx:
+#     print(i)
+
+# Change the default setting
+
+c = calendar.Calendar()
+# for i in c.iterweekdays():
+#     print(i, end=' ')
+
+m = calendar.Calendar()
+for i in m.itermonthdates(2022, 11):
+    print(i)
+print(calendar.monthrange(2022, 11))
